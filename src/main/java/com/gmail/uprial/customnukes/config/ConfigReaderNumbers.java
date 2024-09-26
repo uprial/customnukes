91937c621cb045bb36d0d9849da13f161e81d7d8
Ilya Roslyakov <i_roslyakov@wargaming.net>
2024-09-26 21:37:21 +0300

    Aligned getDouble with other my plugins

--- src/main/java/com/gmail/uprial/customnukes/config/ConfigReaderNumbers.java
+++ src/main/java/com/gmail/uprial/customnukes/config/ConfigReaderNumbers.java
@@ -82,22 +82,23 @@ public final class ConfigReaderNumbers {
         } else if ((! config.isDouble(key)) && (! config.isInt(key))) {
             throw new InvalidConfigException(String.format("A %s is not a double", title));
         } else {
-            double doubleValue = config.getDouble(key);
-
-            //noinspection IfStatementWithTooManyBranches
-            if(!isLengthOfLeftPartOfDoubleGood(doubleValue)) {
-                throw new InvalidConfigException(String.format("A left part of %s has too many digits", title));
-            } else if(!isLengthOfRightPartOfDoubleGood(doubleValue)) {
-                throw new InvalidConfigException(String.format("A right part of %s has too many digits", title));
-            } else if ((min - Double.MIN_VALUE) > doubleValue) {
-                throw new InvalidConfigException(String.format("A %s should be at least %s", title, formatDoubleValue(min)));
-            } else if ((max + Double.MIN_VALUE) < doubleValue) {
-                throw new InvalidConfigException(String.format("A %s should be at most %s", title, formatDoubleValue(max)));
-            } else {
-                value = doubleValue;
-            }
+            value = config.getDouble(key);
+            checkDoubleValue(title, min, max, value);
         }
 
         return value;
+    }
+
+    public static void checkDoubleValue(String title, double min, double max, double doubleValue) throws InvalidConfigException {
+        //noinspection IfStatementWithTooManyBranches
+        if(!isLengthOfLeftPartOfDoubleGood(doubleValue)) {
+            throw new InvalidConfigException(String.format("A left part of %s has too many digits", title));
+        } else if(!isLengthOfRightPartOfDoubleGood(doubleValue)) {
+            throw new InvalidConfigException(String.format("A right part of %s has too many digits", title));
+        } else if ((min - Double.MIN_VALUE) > doubleValue) {
+            throw new InvalidConfigException(String.format("A %s should be at least %s", title, formatDoubleValue(min)));
+        } else if ((max + Double.MIN_VALUE) < doubleValue) {
+            throw new InvalidConfigException(String.format("A %s should be at most %s", title, formatDoubleValue(max)));
+        }
     }
 }
